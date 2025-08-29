1b22fcca14316fad6a3df9f5d571c5bcbacce082
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-08-29 07:56:46 +0530

    Add files via upload

---
+++ coap_python_pc/coap_cleint_pc_multi_req.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiocoap import *
+
+async def send_get(uri):
+    protocol = await Context.create_client_context()
+    req = Message(code=GET, uri=uri)
+
+    try:
+        res = await protocol.request(req).response
+        print(f"Response from {uri}: {res.payload.decode()}")
+    except Exception as e:
+        print(f" Error contacting {uri}:", e)
+
+async def main():
+    await send_get('coap://127.0.0.1/hello')
+    await send_get('coap://127.0.0.1/time')
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
