e7ffba7c22fa76cfc6a6a6f360afc6a4bfcbd64d
Niramai P Nayanar <136973932+NiramaiPNayanar@users.noreply.github.com>
2025-09-01 21:02:56 +0530

    Add files via upload

---
+++ mqtt_cmdline/pub_3.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as paho
+import time
+
+def read_from_imaginary_thermometer():
+    return 100;
+
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+ 
+client = paho.Client()
+client.on_publish = on_publish
+client.connect('localhost', 1883)
+client.loop_start()
+
+while True:
+    temperature = read_from_imaginary_thermometer()
+    (rc, mid) = client.publish('encyclopedia/temperature', str(temperature), qos=1)
+    time.sleep(30)
